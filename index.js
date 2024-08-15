91be640948104531ef95f238d582628e82a481e4
Leorio N. <binhnt@bsscommerce.com>
2024-08-15 09:27:33 +0700

    create demo project

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-// index.js
-function hello(name) {
-  return `Hello, ${name}! Bola here`;
+class Slider {
+  constructor() {
+    let sliderWrapper;
+    let params;
+    let _len = arguments.length;
+    if (_len > 0) this.sliderWrapper = arguments[0];
+    let args = {};
+    for (let  i = 0; i < _len; i++) {
+      args[i] = arguments[i];
+    }
+    console.log('args[0]: ', args)
+    if (_len === 1 && args[0].constructor && typeof args[0] === 'object') {
+      params = args[0];
+    } else {
+      sliderWrapper = args[0];
+      params = args[1];
+      // [sliderWrapper, params] = args;
+    }
+
+    if (!params) params = {};
+
+    if (sliderWrapper) {
+      document.querySelectorAll(sliderWrapper).forEach((containerEl) => {
+        new SliderInstance(containerEl, params);
+      });
+    }
+  }
 }
 
-module.exports = hello;
+class SliderInstance extends HTMLElement {
+  constructor(containerEl, params) {
+    super();
+
+    this.containerEl = containerEl;
+    this.init(params);
+  }
+
+  init(params) {
+    this.spaceBefore = params.spaceBefore;
+    this.nextEl = this.containerEl.querySelector(params.navigation && params.navigation.nextEl ? params.navigation.nextEl : '.swiper-button-next');
+    this.prevEl = this.containerEl.querySelector(params.navigation && params.navigation.prevEl ? params.navigation.prevEl : '.swiper-button-prev');
+    this.slidesPerView = params.slidesPerView;
+    this.slidesPerView = params.slidesPerView;
+  }
+}
+
+let slider = new Slider('.slider-wrapper', {
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  slidesPerView: 3,
+  slidesPerGroup: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 6,
+      slidesPerGroup: 5
+    },
+  },
+})
