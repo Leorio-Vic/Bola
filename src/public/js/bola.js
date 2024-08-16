cb35be69b81e569bfc71cb3b31a71c795be917fb
Leorio N. <binhnt@bsscommerce.com>
2024-08-16 13:45:22 +0700

    Fade Effect

--- src/public/js/bola.js
+++ src/public/js/bola.js
@@ -7,20 +7,17 @@ class SliderInstance {
 
   initData(params) {
     this.spaceBefore = params.spaceBefore;
-    this.nextEl = this.wrapperEl.querySelector(params.navigation && params.navigation.nextEl ? params.navigation.nextEl : '.bola-slider-button__next');
-    this.prevEl = this.wrapperEl.querySelector(params.navigation && params.navigation.prevEl ? params.navigation.prevEl : '.bola-slider-button__prev');
-    this.sliderContainer = this.wrapperEl.querySelector(".bola-slider-container");
-    this.slideItems = this.wrapperEl.querySelectorAll(".bola-slide");
+    this.nextEl = this.wrapperEl.querySelector(`:scope > ${params.navigation && params.navigation.nextEl ? params.navigation.nextEl : '.bola-slider-button__next'}`);
+    this.prevEl = this.wrapperEl.querySelector(`:scope > ${params.navigation && params.navigation.prevEl ? params.navigation.prevEl : '.bola-slider-button__prev'}`);
+    this.sliderContainer = this.wrapperEl.querySelector(":scope > .bola-slider-container");
+    this.slideItems = this.sliderContainer.querySelectorAll(":scope > .bola-slide");
 
     this.firstSlideShowIndex = 0;
     this.lastSlideShowIndex = params.slidesPerView - 1;
 
-    this.sliderWrapperWidth = this.wrapperEl?.offsetWidth ? this.wrapperEl.offsetWidth : 0;
-    this.slideWidth = this.slideItems[0].offsetWidth ? this.slideItems[0].offsetWidth : 0;
-    this.totalSlides = this.slideItems.length;
-
     this.slidesPerView = params.slidesPerView;
     this.slidesPerGroup = params.slidesPerGroup;
+    this.effect = this.wrapperEl.dataset.effect ? this.wrapperEl.dataset.effect : "default";
     this.noLoop = params.noLoop;
   }
 
@@ -28,6 +25,7 @@ class SliderInstance {
     if (this.nextEl) {
       this.nextEl.addEventListener("click", this.handleNext.bind(this));
     }
+
     if (this.prevEl) {
       this.prevEl.addEventListener("click", this.handlePrevious.bind(this));
     }
@@ -40,40 +38,69 @@ class SliderInstance {
   }
 
   handleNext() {
-    // if (!this.nextEl.classList.contains("enabled")) {
-    //   return;
-    // }
+    if (!this.nextEl.classList.contains("enabled")) {
+      return;
+    }
 
-    this.slide(true);
+    this.handleButton(true);
   }
 
   handlePrevious() {
-    // if (!this.prevEl.classList.contains("enabled")) {
-    //   return;
-    // }
-    this.slide(false);
+    if (!this.prevEl.classList.contains("enabled")) {
+      return;
+    }
+
+    this.handleButton(false);
+  }
+
+  handleButton (isNext = true) {
+    switch (this.effect) {
+      case "fade":
+        this.fade(isNext);
+        break;
+      default:
+        this.slide(isNext);
+        break;
+    }
   }
 
   slide(isNext = true) {
-    const gapWidth = parseInt(window.getComputedStyle(this.sliderContainer).columnGap);
-
     const newFirstSlideShowIndex = this.firstSlideShowIndex + this.slidesPerGroup * (isNext ? 1 : -1);
     const lastSlideShowIndex = this.lastSlideShowIndex + this.slidesPerGroup * (isNext ? 1 : -1);
 
-    if (lastSlideShowIndex !== this.slideItems.length + 1) {
-      if (this.slideItems[newFirstSlideShowIndex]) {
-        const targetRect = this.slideItems[newFirstSlideShowIndex].getBoundingClientRect();
-        const containerRect = this.sliderContainer.getBoundingClientRect();
-        let scrollOffset = targetRect.left - containerRect.left;
+    // if (lastSlideShowIndex !== this.slideItems.length + 1) {
+    if (this.slideItems[newFirstSlideShowIndex]) {
+      const targetRect = this.slideItems[newFirstSlideShowIndex].getBoundingClientRect();
+      const containerRect = this.sliderContainer.getBoundingClientRect();
+      let scrollOffset = targetRect.left - containerRect.left;
 
-        this.sliderContainer.style.transform = `translateX(${scrollOffset * -1}px)`;
-        this.firstSlideShowIndex = newFirstSlideShowIndex;
-        this.lastSlideShowIndex = lastSlideShowIndex;
-      }
+      this.sliderContainer.style.transform = `translateX(${scrollOffset * -1}px)`;
+      this.firstSlideShowIndex = newFirstSlideShowIndex;
+      this.lastSlideShowIndex = lastSlideShowIndex;
     }
+    // }
 
     this.prevEl.classList[this.firstSlideShowIndex === 0 ? 'remove' : 'add']('enabled');
     this.nextEl.classList[this.lastSlideShowIndex === this.slideItems.length - 1 ? 'remove' : 'add']('enabled');
+  }
+
+  fade(isNext = true) {
+    let newFirstSlideShowIndex;
+    if (isNext) {
+      newFirstSlideShowIndex = this.firstSlideShowIndex + 1;
+    } else {
+      newFirstSlideShowIndex = this.firstSlideShowIndex - 1;
+    }
+
+    if (this.slideItems[newFirstSlideShowIndex]) {
+      this.slideItems[this.firstSlideShowIndex]?.classList.remove('is-active');
+      this.slideItems[newFirstSlideShowIndex]?.classList.add('is-active');
+      this.firstSlideShowIndex = newFirstSlideShowIndex;
+
+
+      this.prevEl.classList[this.firstSlideShowIndex === 0 ? 'remove' : 'add']('enabled');
+      this.nextEl.classList[this.firstSlideShowIndex === this.slideItems.length - 1 ? 'remove' : 'add']('enabled');
+    }
   }
 }
 
